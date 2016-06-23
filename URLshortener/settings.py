81edf7e5a3ca6087e6891a4c663c0ff642b1b0ea
Devansh Goenka <devanshgoenka97@gmail.com>
2016-06-23 23:47:52 +0530

    Added settings.py example file

--- URLshortener/settings.py
+++ URLshortener/settings.py
@@ -1,9 +1,7 @@
 """
 Django settings for URLshortener project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
